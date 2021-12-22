ec11b5b859951335f4785b26474f69bc8c556688
lizkavalski <lizkavalski@gmail.com>
2021-12-22 13:34:40 -0800

    add get and post

--- src/rouates/v1.js
+++ src/rouates/v1.js
@@ -6,15 +6,24 @@ const modelsMiddleware = require('../middleware/model.js');
 
 router.param('model', modelsMiddleware);
 
+router.get('/', datapage);
 router.get('/:model', handleGetAll);
 router.get('/:model/:id', handleGetOne);
 router.post('/:model', handleCreate);
 router.put('/:model/:id', handleUpdate);
 router.delete('/:model/:id', handleDelete);
 
+async function datapage(req, res){
+  let message = {message:'"There no place like home"- Dorothy(Wizard of Oz)'}
+  res.status(200).json(message)
+}
+
+
 async function handleGetAll(req, res) {
   let allRecords = await req.model.get();
-  res.status(200).json(allRecords);
+  let message = {message:'"“Aw, you guys made me ink.”- Pearl (Finding Nemo.)', allRecords}
+
+  res.status(200).json(message);
 }
 
 async function handleGetOne(req, res) {
@@ -26,6 +35,8 @@ async function handleGetOne(req, res) {
 async function handleCreate(req, res) {
   let obj = req.body;
   let newRecord = await req.model.create(obj);
+  let message = {message:'"“Bbippity boppity boo.”- Fairy Godmother (Cinderella.)'}
+
   res.status(201).json(newRecord);
 }
 
