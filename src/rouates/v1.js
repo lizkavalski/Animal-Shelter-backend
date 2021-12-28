349befb69ae00412eab4985173c2a854c2eaf667
lizkavalski <lizkavalski@gmail.com>
2021-12-28 14:17:35 -0800

    small edit

--- src/rouates/v1.js
+++ src/rouates/v1.js
@@ -45,7 +45,7 @@ async function handleUpdate(req, res) {
   const id = req.params.id;
   const obj = req.body;
   let updatedRecord = await req.model.update(id, obj)
-  let message ={message:"“Hockety pockety wockety wack! Odds and ends and bric-a-brac!”- Merlin(The Sword in the Stone", updatedRecord}
+  let message ={message:"“Hockety pockety wockety wack! Odds and ends and bric-a-brac!”- Merlin(The Sword in the Stone)", updatedRecord}
   res.status(200).json(message);
 }
 
